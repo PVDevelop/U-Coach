c0b8e6d33fb50c22f2094af2dede0698171630eb
beetlewar <beetlewar@mail.ru>
2016-03-23 07:49:48 +0300

    Тестовое соединение с FB

---
+++ TestSSO/src/WebApplication1/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace WebApplication1.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Facebook()
+        {
+            var uri = string.Format(
+                "https://www.facebook.com/dialog/oauth?client_id={0}&redirect_uri={1}&scope={2}",
+                "1034374253265538",
+                "http://localhost:7788/Auth/FacebookCode/",
+                "public_profile");
+
+            return Redirect(uri);
+        }
+
+        public IActionResult FacebookCode(string code)
+        {
+            var uri = string.Format(
+                  "https://graph.facebook.com/v2.5/oauth/access_token?client_id={0}&redirect_uri={1}&client_secret={2}&code={3}",
+                   "1034374253265538",
+                   "http://localhost:7788/Auth/FacebookCode/",  // !!!
+                   "0d5ba10073168902817f1be6a477b12a",
+                   code);
+
+            var httpClient = new HttpClient();
+            var response = httpClient.GetAsync(uri);
+            var res = response.Result;
+            if(res.IsSuccessStatusCode)
+            {
+                var bytes = res.Content.ReadAsByteArrayAsync().Result;
+                var str = Encoding.UTF8.GetString(bytes);
+                // распарсить и вернуть токен
+            }
+            else
+            {
+                // ошибка
+            }
+
+            return PartialView("AuthResultPartial");
+        }
+    }
+}
---
+++ TestSSO/src/WebApplication1/Startup.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Data.Entity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            // Set up configuration sources.
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets();
+
+                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            // To configure external authentication please see http://go.microsoft.com/fwlink/?LinkID=532715
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
