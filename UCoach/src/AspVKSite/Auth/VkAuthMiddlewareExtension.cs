3f0448a4064681ae620b2efa64711c769f09898f
Vetrov Dmitry <dim4kk@yandex.ru>
2016-04-06 22:31:34 +0300

    #1

---
+++ UCoach/src/AspVKSite/Auth/VkAuthMiddlewareExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspVKSite.Auth
+{
+    public static class VkAuthMiddlewareExtension
+    {
+        public static IApplicationBuilder UseVkAuth(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<VKAuthMiddleware>();
+        }
+    }
+}
