19b0181b1e3c8ca0237838a95a291b832b343fd6
Vetrov Dmitry <dim4kk@yandex.ru>
2016-04-06 22:32:38 +0300

    #1

---
+++ UCoach/src/AspVKSite/Auth/VKAuthMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspVKSite.Auth
+{
+    public class VKAuthMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public VKAuthMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Headers["Authorization"] == "Basic aGFicmE6aGFicg==")
+            {
+                context.Response.StatusCode = 200;
+                await _next.Invoke(context);
+            }
+            else
+            {
+                context.Response.StatusCode = 401;
+                context.Response.Headers.Add("WWW-Authenticate", "Basic realm=\"localhost\"");
+            }
+        }
+    }
+}
